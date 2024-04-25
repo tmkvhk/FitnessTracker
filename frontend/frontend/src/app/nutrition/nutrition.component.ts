64912740b3b1c28c97e344710932564262d293a5
Travis Kelley <tmkvhk@umsystem.edu>
2024-04-24 23:19:44 -0400

    1st

---
+++ frontend/frontend/src/app/nutrition/nutrition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nutrition',
+  standalone: true,
+  imports: [],
+  templateUrl: './nutrition.component.html',
+  styleUrl: './nutrition.component.css'
+})
+export class NutritionComponent {
+
+}
