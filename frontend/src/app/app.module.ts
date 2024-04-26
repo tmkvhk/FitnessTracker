1aedebc5a9a7cc652b900bc789544dd15e567004
EjamesH <ejameshinshaw@gmail.com>
2024-04-26 14:07:08 -0500

    added components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NutritionComponent } from './nutrition/nutrition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NutritionComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/nutrition/nutrition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nutrition',
+  templateUrl: './nutrition.component.html',
+  styleUrl: './nutrition.component.css'
+})
+export class NutritionComponent {
+
+}
